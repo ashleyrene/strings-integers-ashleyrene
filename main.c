1e76b3f652bd345247706e72058398a18de3892b
ashleyrene <ashleyrashidian@gmail.com>
2025-03-27 09:36:51 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "hw09.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// suggested testing function declaration
+static void check_string_to_long(const char *string);
+
+int main(int argc, char **argv) {
+  errno = 0;
+
+  fprintf(stderr, "Welcome to ECE264, we are working on HW09!\n\n");
+
+  // test here
+
+  return EXIT_SUCCESS;
+}
